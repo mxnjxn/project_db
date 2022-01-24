e9aa54fdf949bbc0bf6971bd0ef4672f4c682e4a
mxnjxn <choe86898@gmail.com>
2022-01-24 16:17:27 +0900

    ..

--- mysql/exc/exc.sql
+++ mysql/exc/exc.sql
@@ -17,5 +17,63 @@ create table member2 (
     regdate datetime
 );
 
+create table caffee (
+	seq int,
+    caffee_name varchar(100),
+    number_table int,
+    number_chair int,
+    address varchar(100),
+    region int,
+    chief_name varchar(100)
+);
+
+-- table delete
 drop table member2;
 drop table member;
+
+-- data insert
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	1
+    ,'별다방'
+    ,1
+    ,2
+    ,'서울시 어디쯤'
+    ,1
+    ,'장동건'
+);
+-- varchar 는 ' 나 " 로 감싼다
+-- int 는 사용 안해도 된다
+-- 컴마는 앞쪽을 선호a
+
+-- data inquiry
+select * from caffee;
+
+select * from caffee where number_table=9;
+
+select * from caffee where caffee_name='버거킹';
+
+select * from caffee where caffee_name= "멕도날드" and number_chair= 43 and number_table = 9;
+
+select * from caffee where caffee_name= "이디야" or number_chair= 43;
+
+-- int 형태에만 해당된다sss
+select * from caffee where region > 4;
+select * from caffee where region < 4;
+select * from caffee where region >=4;
+select * from caffee where region <=4;
+select * from caffee where region <> 4;
+
+
+-- varchar 형태메만 해당된다.
+select * from caffee where caffee_name like '%디%';
+select * from caffee where caffee_name like '롯데리%';
+select * from caffee where caffee_name like '%데리아';
+-- pk 값에는 같은 값이 들어갈 수 없음.
--- mysql/exc/exc2.sql
+++ mysql/exc/exc2.sql
@@ -8,6 +8,20 @@ create table caffee (
     number_table int,
     number_chair int,
     address varchar(100),
-    region varchar(100),
+    region int,
     chief_name varchar(100)
 );
+
+CREATE TABLE IF NOT EXISTS.`caffee` (
+  `seq` INT UNSIGNED NOT NULL,
+  
+  `caffee_name` VARCHAR(100) NULL,
+  `number_table` INT NULL,
+  `number_chair` INT NULL,
+  `address` VARCHAR(100) NULL,
+  `region` INT NULL,
+  `chief_name` VARCHAR(100) NULL,
+  PRIMARY KEY (`seq`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci
