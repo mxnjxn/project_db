d9cc59bab94778eb4f5cb1a296ec95ef32f7d262
mxnjxn <choe86898@gmail.com>
2022-01-24 16:18:00 +0900

    .

---
+++ mysql/exc/exc3.sql
@@ -0,0 +1,182 @@
+select * from caffee;
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	1
+    ,'별다방'
+    ,1
+    ,2
+    ,'서울시 어디쯤'
+    ,1
+    ,'장동건'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	2
+    ,'빽다방'
+    ,3
+    ,5
+    ,'경기도 어디쁨'
+    ,3
+    ,'일론 머스크'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	3
+    ,'투써믚ㄹ레이쓰'
+    ,6
+    ,3
+    ,'대구시 어디쯤'
+    ,4
+    ,'이건희'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	4
+    ,'투떼주르'
+    ,9
+    ,8
+    ,'가천시 어디쯤'
+    ,89
+    ,'이재용'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	5
+    ,'이디야'
+    ,7
+    ,21
+    ,'대전시 어디쯤'
+    ,4
+    ,'강동원'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	6
+    ,'버거킹'
+    ,8
+    ,23
+    ,'청남 어디쯤'
+    ,9
+    ,'원빈'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	7
+    ,'멕도날드'
+    ,9
+    ,43
+    ,'평양 어디쯤'
+    ,8
+    ,'김정은'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	8
+    ,'맘스터치'
+    ,9
+    ,10
+    ,'제주도 어디쯤'
+    ,89
+    ,'장보고'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	9
+    ,'롯데리아'
+    ,32
+    ,43
+    ,'경기도 어디쯤'
+    ,78
+    ,'서장훈'
+);
+
+insert into caffee (
+	seq
+	,caffee_name
+	,number_table
+	,number_chair
+	,address
+	,region
+	,chief_name 
+) values (
+	10
+    ,'벤츠'
+    ,89
+    ,230
+    ,'독도 어디쯤'
+    ,234
+    ,'이순신'
+);
+select * from caffee;
